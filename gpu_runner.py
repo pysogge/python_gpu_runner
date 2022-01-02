b84df7d775695441f6e55c5640ec75f5ed98ce56
Zwerb-Nuc <sy@zwerb.com>
2022-01-02 11:36:34 -0500

    first commit

---
+++ gpu_runner.py
@@ -0,0 +1,26 @@
+from numba import vectorize, jit, cuda 
+import numpy as np 
+# to measure exec time 
+from timeit import default_timer as timer 
+
+# normal function to run on cpu 
+def func(a):                                 
+    for i in range(10000000): 
+        a[i]+= 1    
+
+# function optimized to run on gpu 
+@vectorize(['float64(float64)'], target ="cuda")                         
+def func2(x): 
+    return x+1
+
+if __name__=="__main__": 
+    n = 500000000                            
+    a = np.ones(n, dtype = np.float64) 
+
+    start = timer() 
+    func(a) 
+    print("1x without GPU:", timer()-start)     
+
+    start = timer() 
+    func2(a) 
+    print("50x with GPU:", timer()-start) 
